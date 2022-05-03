034dc0571173973c2c84a8e057d09f1d920dfbe5
Partha Pratim Das <dasparthapratim02@gmail.com>
2022-05-04 00:25:23 +0530

    c

---
+++ ndtv_clone/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import axios from "axios"
+import {useEffect} from "react-router-dom"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      useEffect(()=>{},[])
+      axios.get()
+     
+    </div>
+  )
+}
+
+export default App
