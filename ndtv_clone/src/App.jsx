8909fdb796edb98a2f1bf68f8132a356e25fe25d
Partha Pratim Das <dasparthapratim02@gmail.com>
2022-05-04 00:45:36 +0530

    x

--- ndtv_clone/src/App.jsx
+++ ndtv_clone/src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import axios from "axios"
-import {useEffect} from "react-router-dom"
+import {useEffect} from "react"
 
 function App() {
   const [count, setCount] = useState(0)
 
+  useEffect(()=>{
+  console.log("tes")
+  },[])
   return (
     <div className="App">
-      useEffect(()=>{},[])
-      axios.get()
      
     </div>
   )
