3387f5abc30c9023a3dc3aae391d859a15659649
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-05-05 13:16:09 +0530

    done

--- ndtv_clone/src/components/Midnav/Offbeat.jsx
+++ ndtv_clone/src/components/Midnav/Offbeat.jsx
@@ -1,24 +1,49 @@
 import axios from "axios";
 import { useState ,useEffect } from "react";
 
-import {Smallcart}  from "./smallcart";
+import {Smallcart, Trending}  from "./smallcart";
+import "./Ofbeat.css"
 
 export const Offbeat =()=>{
-    const [count, setCount] = useState([])
+    const [count, setCount] = useState([]);
+    const [trend,setTrend] =useState([]);
 
     useEffect(()=>{
     axios.get("https://ndtvnews-api.herokuapp.com/general?category=values(offbeat)").then((res)=>{setCount(res.data.news[0].articles)})
-    }, [])
+    }, []);
+
+    useEffect(()=>{
+        axios.get("https://ndtvnews-api.herokuapp.com/general?category=values(entertainment,india)").then((res)=>{setTrend(res.data.news[0].articles)})
+        }, [])
     
     console.log(count)
+    console.log(trend)
     return(
-        <div>home home
+        <>
+        <div><h1 style={{textAlign:"left" ,marginLeft:"200px"}}>Offbeat</h1>
+          <div className="containerdiv">
 
+          <div className="left">
+              <div className="heading"><b>TENDING</b></div>
             {
-                count.map((e)=>{
-                    return <Smallcart image={e.image_url} headline={e.headline} url={e.url} description={e.description}></Smallcart>
-                })
+             trend.map((e)=>{
+                return <Trending image={e.image_url} headline={e.headline} ></Trending>
+               })
             }
         </div>
+
+                           
+            <div className="right">
+                {
+                count.map((e)=>{
+                    return <Smallcart image={e.image_url} headline={e.headline} url={e.url} description={e.description} posted_date={e.posted_date}></Smallcart>
+                })
+                }
+            </div>
+
+            
+            </div>   
+        </div>
+        </>
     )
 }
--- ndtv_clone/src/components/Midnav/smallcart.jsx
+++ ndtv_clone/src/components/Midnav/smallcart.jsx
@@ -1,5 +1,5 @@
 import "./smallcart.css"
-export const Smallcart =({image,headline,url,description})=>{
+export const Smallcart =({image,headline,url,description,posted_date})=>{
 
 
     return <>
@@ -7,8 +7,24 @@ export const Smallcart =({image,headline,url,description})=>{
         <img src={image} alt="" />
         <div>
             <h3>{headline}</h3>
-            <h5>{description}</h5>
+            <p>{"Edited by Bhavya Sukheja |"} {posted_date} </p>
+            <p>{description}</p>
             <h6><a href={url}>{"For more click here.."}</a></h6>
+        </div>
+    </div>
+    
+    </>
+};
+
+export const Trending =({image,headline})=>{
+
+
+    return <>
+    <div className="mainsm">
+        <img src={image} alt="" />
+        <div>
+            <h2>{headline}</h2>
+           
         </div>
     </div>
     
