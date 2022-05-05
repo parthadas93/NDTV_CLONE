2aaaf8d2b0a6100c7a3735715538a08a18b7f360
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-05-06 01:07:15 +0530

    icon added

--- ndtv_clone/src/components/Navbar.jsx
+++ ndtv_clone/src/components/Navbar.jsx
@@ -25,6 +25,30 @@ export const Nav=()=>{
         {title:"TRENDS",to:"/trends"},
         {title:"PHOTOS",to:"/photos"},
         
+    ];
+
+    const icontab=[
+        {
+            link:"https://www.facebook.com/ndtv?campaign_id=237467320636",
+            img:"https://cdn-icons-png.flaticon.com/128/5968/5968764.png",
+        },
+        {
+            link:"https://twitter.com/ndtv",
+            img:"https://cdn-icons-png.flaticon.com/128/733/733579.png",
+        },
+        {
+            link:"https://www.kooapp.com/profile/ndtv",
+            img:"https://www.kooapp.com/assets/2d634360.svg",
+        },
+        {
+            link:"",
+            img:"https://cdn-icons.flaticon.com/png/128/2523/premium/2523586.png?token=exp=1651779061~hmac=d63c0e9d315ef5c7ab7497cbb64d5422",
+        },
+        {
+            link:"",
+            img:"https://cdn-icons-png.flaticon.com/128/2645/2645897.png",
+        },
+
     ]
     
     return(
@@ -41,6 +65,12 @@ export const Nav=()=>{
                     })
                 }
             <button style={{background:"rgb(62,62,62)" ,border:"none",color:"white"}} onClick={()=>dispatch(toggleshow(!hiden))}>{'...'}</button>
+            {
+                icontab.map((e)=>
+                   <a href={e.link}><img className="icon" src={e.img} alt="" /></a> 
+                )
+            }
+            
             
         </div>
     )
